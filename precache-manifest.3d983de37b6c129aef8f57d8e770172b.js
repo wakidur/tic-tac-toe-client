0ab51ae8436f30062af1a31603611f66e39200b0
wakidur <wakidur@gmail.com>
2020-08-10 12:33:46 +0600

    Updates

---
+++ precache-manifest.3d983de37b6c129aef8f57d8e770172b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee085e54a43a3c1d32355454a32163dc",
+    "url": "/tic-tac-toe-client/index.html"
+  },
+  {
+    "revision": "73668f89b9b4d8f9d4f4",
+    "url": "/tic-tac-toe-client/static/js/2.b43e4675.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/tic-tac-toe-client/static/js/2.b43e4675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f15c695a7afddefd9c",
+    "url": "/tic-tac-toe-client/static/js/main.ef13df15.chunk.js"
+  },
+  {
+    "revision": "8c0a5380b64c2b59e3d4",
+    "url": "/tic-tac-toe-client/static/js/runtime-main.eaf78a55.js"
+  }
+]);
